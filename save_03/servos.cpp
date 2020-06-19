bf38498a8e3e1ee4d33c2e46549df8600df2e78a
Steve Conklin <steve@conklinhouse.com>
2020-06-18 19:40:00 -0500

    getting closer

---
+++ save_03/servos.cpp
@@ -0,0 +1,85 @@
+#include "application.h"
+#include "servos.h"
+/*
+ * Coded for the Adafruit 1411 servo controller,
+ * which uses a PCA9685 I2C PWM LED controller.
+ */
+double countsPerUsec = 4096.0/20000.0;
+int servoPosition1;
+int servoPosition2;
+
+void servoHandleTimer()
+{
+    servoPosition1 = (analogRead(SERVO_1_FEEDBACK_PIN) * 360) / 4096;
+    servoPosition2 = (analogRead(SERVO_2_FEEDBACK_PIN) * 360) / 4096;
+}
+
+Timer servotimer(10, servoHandleTimer);
+
+void servoInit(void) {
+    // Calculate Prescale
+    int prescale = (SRV_OSCILLATOR_FREQUENCY/(4096 * SRV_UPDATE_RATE))-1;
+
+    servoSetPrescale(prescale);
+    servoSetAutoIncrement(true);
+    //servotimer.start();
+}
+
+void servoSleep(void) {
+    unsigned char regval = servoReadSingleRegister(SRV_REG_MODE_1);
+    servoWriteSingleRegister(SRV_REG_MODE_1, regval |= SRV_SLEEP_BIT);
+    delay(50);
+}
+
+void servoWake() {
+    unsigned char regval = servoReadSingleRegister(SRV_REG_MODE_1);
+    servoWriteSingleRegister(SRV_REG_MODE_1, regval &= ~SRV_SLEEP_BIT);
+}
+
+void servoSetPrescale(unsigned char prescale) {
+    servoSleep();
+    servoWriteSingleRegister(SRV_REG_PRESCALE, prescale);
+    servoWake();
+}
+
+void servoSetAutoIncrement(bool ai) {
+    unsigned char regval = servoReadSingleRegister(SRV_REG_MODE_1);
+    servoWriteSingleRegister(SRV_REG_MODE_1, regval |= SRV_AI_BIT);
+}
+
+void servoWriteSingleRegister(int regnum, unsigned char regval) {
+    Wire.beginTransmission(SRV_I2C_ADDR);
+    Wire.write(regnum);
+    Wire.write(regval);
+    Wire.endTransmission(true);        // stop transmitting
+}
+
+unsigned char servoReadSingleRegister(int regnum) {
+    Wire.beginTransmission(SRV_I2C_ADDR);
+    Wire.write(regnum);
+    Wire.endTransmission(true);        // stop transmitting
+    Wire.requestFrom(SRV_I2C_ADDR, 1);    // request 1 byte
+    if(Wire.available()) {   // slave may send less than requested
+        return( Wire.read());
+    } else {
+        return 0;
+    }
+}
+
+void servoSetChannelMicroseconds(int channum, long usecs) {
+    uint16_t offvalue;
+    unsigned char regaddr;
+
+    offvalue = round(double(usecs) * countsPerUsec);
+    regaddr = SRV_REG_CHAN_BASE + ((channum-1)*4);
+
+    // Always set start time to zero
+    // Assume register address autoincrement is on
+    Wire.beginTransmission(SRV_I2C_ADDR);
+    Wire.write(regaddr);
+    Wire.write(0x00);
+    Wire.write(0x00);
+    Wire.write(offvalue & 0xFF);
+    Wire.write((offvalue & 0x0F00) >> 8);
+    Wire.endTransmission(true);
+}
---
+++ save_03/servos.h
@@ -0,0 +1,67 @@
+// Internal oscillator frequency varies between ICs, calibrate this to your part
+#define SRV_OSCILLATOR_FREQUENCY (26450000)
+#define SRV_UPDATE_RATE 50 // 50 Hz update for servos
+
+/* Default controller I2C address ix 0x40
+ * Default all-call LED address is 0xE0
+ * Default SUBADDR1 address is 0xE2
+ * Default SUBADDR2 address is 0xE4
+ * Default SUBADDR3 address is 0xE8
+ */
+#define SRV_I2C_ADDR 0x40
+#define SRV_REG_MODE_1 0
+#define SRV_REG_MODE_2 1
+#define SRV_REG_PRESCALE 254
+// PRESCALE can only be set when SLEEP = 1
+#define SRV_REG_CHAN_BASE 6
+
+#define SRV_SLEEP_BIT 0x10
+#define SRV_AI_BIT 0x20
+
+#define SERVO_1_FEEDBACK_PIN A0
+#define SERVO_2_FEEDBACK_PIN A1
+
+#define SRV_STOP_US 1500
+// 1465 moves one servo but not the other
+#define SRV_CW_VERY_SLOW_US 1460
+#define SRV_CW_SLOW_US 1465
+#define SRV_CW_MED_US 1360
+#define SRV_CW_FAST_US 1280
+#define SRV_CCW_VERY_SLOW_US 1550
+#define SRV_CCW_SLOW_US 1580
+#define SRV_CCW_FAST_US 1720
+
+// Mode register 1 default
+// 0x11
+// 0        RESTART (0 = disabled)
+//  0       EXTCLK (1 = enabled)
+//   0      AUTOINCREMENT (1 enables register autoincrement)
+//    1     SLEEP (1 = Low Power Mode)
+//     0    SUB1 (1 = responds to I2C subaddress 1)
+//      0   SUB2
+//       0  SUB3
+//        1 ALLCALL (1=responds to AllCall I2C address)
+
+// Mode register 2 default
+// 0x08
+// 0        RESERVED, read only
+//  0       RESERVED, read only
+//   0      RESERVED, read only
+//    0     INVERT (1 = invert output state)
+//     0    OCH (0 = output changes state on STOP command, 1 = output changes state on ACK)
+//      1   OUTDRV (0 = open-drain, 1 = totem pole)
+//       0  OUTNE (see data sheet)
+//        0 OUTNE 
+
+extern int servoPosition1;
+extern int servoPosition2;
+
+
+void servoInit(void);
+void servoSleep(void);
+void servoWake(void);
+void servoSetPrescale(unsigned char);
+void servoSetAutoIncrement(bool);
+void servoWriteSingleRegister(int, unsigned char);
+unsigned char servoReadSingleRegister(int);
+void servoSetChannelMicroseconds(int, long);
